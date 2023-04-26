d0769064bcba155b482f83c61d18545567ae5121
zyx0208 <zyx0208@naver.com>
2023-04-26 17:58:54 +0900

    test1
    test1

---
+++ Source/test/test.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, test, "test" );
+ 
